91037c19594b6be48a2d33ad967c1bba6dc75e97
ieee0824 <ieee0824x@gmail.com>
2017-10-04 11:51:00 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,3 @@
-// Copyright 2017 The Bazel Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// The skylark command interprets a Skylark file.
-// With no arguments, it starts a read-eval-print loop (REPL).
 package main
 
 import (
